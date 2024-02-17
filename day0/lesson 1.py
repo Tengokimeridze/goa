db214870dfb348fa9103242122bbf8fb1c3a448e
Tengokimeridze <160333915+Tengokimeridze@users.noreply.github.com>
2024-02-17 20:10:27 +0400

    Add files via upload

---
+++ day0/lesson 1.py
@@ -0,0 +1,50 @@
+from turtle import*
+
+#we want to paint a house
+
+#step:1 draw a square
+
+width(7)
+color("cyan")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#end of square
+
+#drawing a door
+forward(70)
+color("red")
+left(90)
+forward(120)
+right(90)
+forward(80)
+right(90)
+forward(120)
+
+penup()
+goto(200,200)
+pendown()
+
+color("yellow")
+right(120)
+forward(150)
+left(75)
+forward(100)
+
+
+
+
+
+
+
+
+
+exitonclick()
